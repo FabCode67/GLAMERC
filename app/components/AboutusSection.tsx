4220d585edb1bad552edaf859257a8e42cee6848
FabCode67 <mwanafunzifabrice@gmail.com>
2025-01-10 11:56:32 +0200

    quick fix

--- app/components/AboutusSection.tsx
+++ app/components/AboutusSection.tsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
     <div id="about" className="relative max-w-7xl lg:flex-row flex flex-col gap-5 my-auto mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-16 sm:pt-20 sm:pb-20">
       <div className="md:w-1/2 w-full  my-auto flex flex-col space-y-4">
         <h1 className="md:text-5xl text-2xl font-bold absolute md:top-12 -top-5">About Us</h1>
-        <Image src="/service.jpg" alt="About Us" width={500} height={500} className="rounded-md w-full object-fill" />
+        <Image src="/images/service.jpg" alt="About Us" width={500} height={500} className="rounded-md w-full object-fill" />
         <div className="bg-teal-500 rounded-md my-auto mt-auto justify-center flex">
 
           <div className="bg-teal-500 rounded-md md:p-4 p-2">
